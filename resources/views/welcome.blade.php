089e469eface137c67b11eca1bd78ce81eb01b55
alpachinof <m.chahkandi2@gmail.com>
2022-12-12 10:36:48 +0330

    fixed dashboard cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,23 +12,22 @@
     </head>
     <body>
         @include('alerts')
-        <div class="mt-16 flex flex-wrap gap-4 w-full text-center">
-            <div class="w-64 h-32 bg-red-400 p-4">
-                <h1>کاربران ثبت نام</h1>
-                
-                <span>{{$registerusers}}</span>
+        <div class="mt-16 px-4 flex justify-center lg:justify-start flex-wrap gap-4 w-full text-center">
+            <div class="w-full h-32 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">کاربران ثبت نام</h5>
+                <span class="mb-2 font-bold text-gray-700">{{$registerusers}}</span>
             </div>
-            <div class="w-64 h-32 bg-red-400 p-4">
-                <h1>کاربران اداری</h1>
-                <span>{{$officeusers}}</span>
+            <div class="w-full h-32 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">کاربران مدیر</h5>
+            <span class="mb-3 font-bold text-gray-700">{{$admins}}</span>
             </div>
-            <div class="w-64 h-32 bg-red-400 p-4">
-                <h1>کاربران مدیر</h1>
-                <span>{{$admins}}</span>
+            <div class="w-full h-32 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">کاربران اداری</h5>
+            <span class="mb-3 font-bold text-gray-700">{{$officeusers}}</span>
             </div>
-            <div class="w-64 h-32 bg-red-400 p-4">
-                <h1>دانشجویان</h1>
-                <span>4700</span>
+            <div class="w-full h-32 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">دانشجویان</h5>
+            <span class="mb-3 font-bold text-gray-700">2600</span>
             </div>
 
         </div>
