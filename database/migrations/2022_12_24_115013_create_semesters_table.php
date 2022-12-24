794ddcc16f19fb22a3b6c7d7450d2b475bbf899d
alpachinof <m.chahkandi2@gmail.com>
2022-12-24 16:57:07 +0330

    added semester migration

---
+++ database/migrations/2022_12_24_115013_create_semesters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->id();
+            $table->char('year', 4);
+            $table->tinyInteger('semester');
+            $table->char('code', 3);
+            $table->boolean('current');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semesters');
+    }
+};
